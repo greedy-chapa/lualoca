a25d8f9fbdab0e35ab80bbec2f1b5fa8a87076bd
Chapa <greedy.chapa@gmail.com>
2025-03-23 02:10:26 +0500

    updated link to funlutab module

--- example/exapmple.lua
+++ example/exapmple.lua
@@ -1,21 +1,21 @@
 ---@type lualoca
 local lualoca=require('path/to/this/module')
----@type funlutab https://github.com/Mantyi-Studio/funlutab
+---@type funlutab https://github.com/greedy-chapa/funlutab
 local funlutab=require('path/to/funlutab.lua')
 ---@type table https://github.com/rxi/json.lua
 local json=require('path/to/json.lua')
 
----Creating instance
+--Creating instance
 local instance=lualoca.Instance(funlutab, json, '.../text/', '.../text/params.json')
----Before using you have to set language
+--Before using you have to set language
 instance:set_language('en')
----To get a text with no parameters (or if you want them all to be nil) you shouldn't set the `values` field
+--To get a text with no parameters (or if you want them all to be nil) you shouldn't set the `values` field
 io.write(instance:get_text({path={'commands', 'language'}}))
 instance:set_language(io.read())
----If the lualoca object only contains a path field, you can just specify only that ({'path', '...'})
+--If the lualoca object only contains a path field, you can just specify only that ({'path', '...'})
 io.write(instance:get_text({'name'}))
 local name=io.read()
----If the text contains only 1 parameter you can name it '' (empty) and use values=value
+--If the text contains only 1 parameter you can name it '' (empty) and use values=value
 print(instance:get_text({path={'hi'}, values=name}))
 print(instance:get_text({'commands', 'list'}))
 
@@ -38,7 +38,7 @@ while true do
 		local price=io.read()
 		io.write(instance:get_text({'commands', 'price', 'currency'}))
 		local currency=io.read()
-		---If the text contains more than 1 parameter (or 1 but not named '') you must use values={param=value...}
+		--If the text contains more than 1 parameter (or 1 but not named '') you must use values={param=value...}
 		print(instance:get_text({path={'commands', 'price', 'label'}, values={n=product, p=price, c=currency}}))
 	elseif command=='language' then
 		io.write(instance:get_text({'commands', 'language'}))
